487b3469b4e5e975cbe584e67b3a6f1444e24bf3
shapooo <shabojia@outlook.com>
2023-11-19 16:20:28 +0800

    implement network

--- src/network.rs
+++ src/network.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::io::Write;
 use std::net::{SocketAddr, TcpListener, TcpStream};
 use std::sync::{
     mpsc::{channel, Receiver, Sender, TryRecvError},
@@ -6,6 +7,7 @@ use std::sync::{
 };
 use std::thread::spawn;
 
+use borsh::{BorshDeserialize, BorshSerialize};
 use hotstuff_rs::{
     messages::Message as HsMessage,
     networking,
@@ -13,11 +15,14 @@ use hotstuff_rs::{
 };
 
 #[derive(Clone)]
+#[allow(dead_code)]
 pub struct NetworkImpl {
     validator_set: Arc<RwLock<ValidatorSet>>,
-    peer_address: Arc<RwLock<HashMap<PublicKeyBytes, SocketAddr>>>,
+    peer_addresses: Arc<RwLock<HashMap<PublicKeyBytes, SocketAddr>>>,
     tx_sender: Sender<Message>,
     rx_receiver: Arc<Mutex<Receiver<Message>>>,
+    sender_close: Sender<()>,
+    receiver_close: Sender<()>,
     host_addr: SocketAddr,
 }
 
@@ -26,42 +31,63 @@ impl NetworkImpl {
         let peer_address = Arc::new(RwLock::new(initial_peers));
         let (tx_sender, tx_receiver) = channel::<Message>();
         let (rx_sender, rx_receiver) = channel::<Message>();
+        let (sender_close, sender_close_rx) = channel::<()>();
+        let (receiver_close, receiver_close_rx) = channel::<()>();
 
         // TODO: impl receiving thread
         spawn(move || {
             let listener = TcpListener::bind(host_addr).unwrap();
             loop {
+                match receiver_close_rx.try_recv() {
+                    Ok(()) => return,
+                    Err(TryRecvError::Empty) => {}
+                    Err(TryRecvError::Disconnected) => {
+                        return;
+                    }
+                }
                 match listener.accept() {
-                    Ok((_socket, addr)) => println!("new client: {addr:?}"),
+                    Ok((mut socket, _addr)) => {
+                        let msg = Message::deserialize_reader(&mut socket).unwrap();
+                        rx_sender.send(msg).unwrap();
+                    }
                     Err(e) => println!("couldn't get client: {e:?}"),
                 }
             }
         });
         {
-            // TODO: send msg
             // TODO: exception handle when connect
             let peer_address = peer_address.clone();
             spawn(move || {
-                let mut connection_map: HashMap<PublicKeyBytes, TcpStream> = HashMap::new();
+                let mut connections: HashMap<PublicKeyBytes, TcpStream> = HashMap::new();
                 loop {
+                    match sender_close_rx.try_recv() {
+                        Ok(()) => return,
+                        Err(TryRecvError::Empty) => {}
+                        Err(TryRecvError::Disconnected) => {
+                            return;
+                        }
+                    }
                     let msg = tx_receiver.recv().unwrap();
-                    let stream = match connection_map.get(&msg.0) {
+                    let mut stream = match connections.get(&msg.0) {
                         Some(stream) => stream,
                         None => {
                             let addr = peer_address.read().unwrap().get(&msg.0).unwrap().clone();
                             let stream = TcpStream::connect(addr).unwrap();
-                            connection_map.insert(msg.0.clone(), stream);
-                            connection_map.get(&msg.0).unwrap()
+                            connections.insert(msg.0.clone(), stream);
+                            connections.get(&msg.0).unwrap()
                         }
                     };
+                    stream.write(&msg.try_to_vec().unwrap()).unwrap();
                 }
             });
         }
         Self {
             validator_set: Arc::new(RwLock::new(ValidatorSet::new())),
-            peer_address,
+            peer_addresses: peer_address,
             tx_sender,
             rx_receiver: Arc::new(Mutex::new(rx_receiver)),
+            sender_close,
+            receiver_close,
             host_addr,
         }
     }
@@ -107,5 +133,61 @@ impl networking::Network for NetworkImpl {
     }
 }
 
+impl Drop for NetworkImpl {
+    fn drop(&mut self) {
+        self.sender_close.send(()).unwrap();
+        self.receiver_close.send(()).unwrap();
+    }
+}
+
 // TODO: add Signature
+#[derive(BorshDeserialize, BorshSerialize)]
 struct Message(PublicKeyBytes, HsMessage);
+
+#[cfg(test)]
+mod network_test {
+    use super::*;
+    use hotstuff_rs::{
+        messages::{Message as HsMessage, ProgressMessage, Proposal},
+        types::{Block, QuorumCertificate},
+    };
+    use networking::Network;
+
+    #[test]
+    fn new_test() {
+        NetworkImpl::new(Default::default(), "127.0.0.1:8082".parse().unwrap());
+    }
+
+    #[test]
+    fn send_receive_test() {
+        let mut peers: HashMap<[u8; 32], SocketAddr> = HashMap::new();
+        let sender_addr = "127.0.0.1:8080".parse().unwrap();
+        let receiver_addr = "127.0.0.1:8081".parse().unwrap();
+        let sender_pubkey = [0; 32];
+        let receiver_pubkey = [1; 32];
+        peers.insert(sender_pubkey, sender_addr);
+        peers.insert(receiver_pubkey, receiver_addr);
+        let mut receiver = NetworkImpl::new(peers.clone(), receiver_addr);
+        let mut sender = NetworkImpl::new(peers, sender_addr);
+        sender.send(
+            receiver_pubkey,
+            HsMessage::ProgressMessage(ProgressMessage::Proposal(Proposal {
+                chain_id: 0,
+                view: 0,
+                block: Block::new(
+                    0,
+                    QuorumCertificate::genesis_qc(),
+                    Default::default(),
+                    Default::default(),
+                ),
+            })),
+        );
+
+        loop {
+            if let Some((key, _msg)) = receiver.recv() {
+                println!("received {:?}", key);
+                break;
+            }
+        }
+    }
+}
