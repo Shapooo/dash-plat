efe39471ffa61023cc74d49e4203126027ed7cf5
shapooo <shabojia@outlook.com>
2023-11-20 13:36:28 +0800

    add config parser

--- src/config.rs
+++ src/config.rs
@@ -1,21 +1,26 @@
+use crate::crypto;
+
 use std::collections::{HashMap, HashSet};
 use std::env::current_exe;
 use std::net::SocketAddr;
 use std::path::Path;
 use std::time::Duration;
 
-use hotstuff_rs::types::PublicKeyBytes;
-
 use anyhow::{anyhow, Result};
-use serde::{Deserialize, Serialize};
+use hotstuff_rs::types::PublicKeyBytes;
+use serde::{Deserialize, Deserializer, Serialize};
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Config {
     pub host_address: SocketAddr,
+    #[serde(deserialize_with = "parse_peer_addresses")]
     pub peer_addresses: HashMap<PublicKeyBytes, SocketAddr>,
+    #[serde(deserialize_with = "parse_validators")]
     pub validators: HashSet<PublicKeyBytes>,
+    #[serde(deserialize_with = "parse_seconds")]
     pub minimum_view_timeout: Duration,
     pub sync_request_limit: u32,
+    #[serde(deserialize_with = "parse_seconds")]
     pub sync_response_timeout: Duration,
 }
 
@@ -50,4 +55,36 @@ impl Config {
             .join("config.yaml");
         Self::from_path(config_path)
     }
+}
+
+fn parse_seconds<'de, D>(d: D) -> Result<Duration, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let secs: u64 = Deserialize::deserialize(d)?;
+    Ok(Duration::from_secs(secs))
+}
+
+fn parse_peer_addresses<'de, D>(d: D) -> Result<HashMap<PublicKeyBytes, SocketAddr>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let pa_map: HashMap<String, SocketAddr> = Deserialize::deserialize(d)?;
+    let res: Result<HashMap<_, _>> = pa_map
+        .into_iter()
+        .map(|(ps, addr)| crypto::publickey_from_base64(&ps).map(|ps| (ps, addr)))
+        .collect();
+    Ok(res.unwrap())
+}
+
+fn parse_validators<'de, D>(d: D) -> Result<HashSet<PublicKeyBytes>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let validators: Vec<String> = Deserialize::deserialize(d)?;
+    let res: Result<HashSet<PublicKeyBytes>> = validators
+        .into_iter()
+        .map(|s| crypto::publickey_from_base64(&s))
+        .collect();
+    Ok(res.unwrap())
 }
