d6724ab90d9a82f52fa3510d59becfa545f02c84
Shapo <ShaboJia@outlook.com>
2023-11-24 11:55:14 +0800

    add basic test configure helper script

---
+++ scripts/basic_test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+SHELL_FOLDER=`cd $(dirname $0); pwd`
+WORKSPACE_FOLDER=`cd ${SHELL_FOLDER}/..; pwd`
+
+cd ${WORKSPACE_FOLDER}
+if [ -d "experiment" ]; then
+    rm -rf experiment
+fi
+mkdir experiment
+cd experiment
+
+cargo build
+cargo run -p dash-tools --bin=config-gen
+
+for i in {0..3}
+do 
+    mkdir -p ${i}/config/peers
+    mv $i.config.yaml ${i}/config/config.yaml
+    mv $i.sec ${i}/config/sec_key
+    for j in {0..3}
+    do
+        cp ${j}.peerconfig.yaml ${i}/config/peers/${j}.peerconfig.yaml
+    done
+    cp ${WORKSPACE_FOLDER}/target/debug/dash-node ${i}/
+done
+
+for i in {0..3}
+do 
+    rm ${i}.peerconfig.yaml
+done
