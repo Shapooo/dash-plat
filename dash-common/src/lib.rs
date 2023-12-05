aa551304807376c7b87dacdcdb7672f2b314a178
shapooo <shabojia@outlook.com>
2023-12-05 15:27:34 +0800

    design types of msg between client and node

--- dash-common/src/lib.rs
+++ dash-common/src/lib.rs
@@ -1,6 +1,19 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 
-#[derive(BorshDeserialize, BorshSerialize)]
-pub struct BlockMsg {
+#[derive(Debug, Clone, BorshSerialize, BorshDeserialize)]
+pub struct NewTransactionRequest {
+    pub id: u64,
     pub data: Vec<u8>,
+}
+
+#[derive(Debug, Clone, BorshSerialize, BorshDeserialize)]
+pub struct TransactionReceipt {
+    pub id: u64,
+    pub result: TransactionResult,
+}
+
+#[derive(Debug, Clone, PartialEq, BorshSerialize, BorshDeserialize)]
+pub enum TransactionResult {
+    Commited,
+    Unaccepted,
 }
