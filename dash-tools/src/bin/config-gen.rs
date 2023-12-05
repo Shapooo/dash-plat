6cc60530dcb59906405b520f53b3a293c2dbcdbd
shapooo <shabojia@outlook.com>
2023-12-05 15:48:43 +0800

    implement function to generate client config, compatible with node config

--- dash-tools/src/bin/config-gen.rs
+++ dash-tools/src/bin/config-gen.rs
@@ -1,9 +1,11 @@
+use dash_client::config::Config as ClientConfig;
 use dash_node::{
     config::{Config, PeerConfig},
     crypto,
 };
 
 use std::io::Write;
+use std::net::SocketAddr;
 use std::path::PathBuf;
 use std::{fs::OpenOptions, time::Duration};
 
@@ -25,6 +27,9 @@ struct Cli {
     /// Start port
     #[arg(short, long, default_value = "3000")]
     pub start_port: u16,
+    /// And generate config file for client
+    #[arg(short, long, default_value = "true")]
+    pub with_client_config: bool,
 }
 
 fn main() -> Result<()> {
@@ -37,12 +42,32 @@ fn main() -> Result<()> {
             .map(|n| gen_keypair_file(cli.output_path.join(n.to_string())))
             .collect::<Result<Vec<_>>>()?;
     } else {
-        if cli.count as u32 + cli.start_port as u32 > u16::MAX as u32 {
+        if cli.count as u32 * 2 + cli.start_port as u32 > u16::MAX as u32 {
             return Err(anyhow!("port overflow"));
         }
         (0..cli.count)
-            .map(|n| gen_config_file(cli.output_path.join(n.to_string()), cli.start_port + n))
+            .map(|n| gen_config_file(cli.output_path.join(n.to_string()), cli.start_port + n * 2))
             .collect::<Result<Vec<_>>>()?;
+    }
+
+    if cli.with_client_config {
+        let config = ClientConfig {
+            node_addrs: (0..cli.count)
+                .into_iter()
+                .map(|i| {
+                    let port = cli.start_port + i * 2 + 1;
+                    ("127.0.0.1:".to_string() + &port.to_string())
+                        .parse::<SocketAddr>()
+                        .unwrap()
+                })
+                .collect(),
+        };
+        let mut client_config = OpenOptions::new()
+            .create(true)
+            .truncate(true)
+            .write(true)
+            .open(&cli.output_path.join("client_config.yaml"))?;
+        client_config.write_all(serde_yaml::to_string(&config)?.as_bytes())?;
     }
     Ok(())
 }
@@ -83,7 +108,8 @@ fn gen_config_file(mut path: PathBuf, port: u16) -> Result<()> {
         validators: Default::default(),
         peer_addresses: Default::default(),
         my_keypair: None,
-        host_address: ("127.0.0.1:".to_string() + &port.to_string()).parse()?,
+        peer_listen_addr: ("127.0.0.1:".to_string() + &port.to_string()).parse()?,
+        client_listen_addr: ("127.0.0.1:".to_string() + &(port + 1).to_string()).parse()?,
         minimum_view_timeout: Duration::from_millis(500),
         sync_request_limit: 100,
         sync_response_timeout: Duration::from_millis(5000),
